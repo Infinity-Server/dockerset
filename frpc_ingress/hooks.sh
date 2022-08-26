157510e189415f03be1d4caa92dac5197cd15b29
springhack <springhack@live.cn>
2022-08-26 16:01:10 +0800

    code adjust

---
+++ frpc_ingress/hooks.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+hooks::on_config() {
+  cat <<EOF
+configVersion: v1
+onStartup: 0
+kubernetes:
+  - apiVersion: crds.dosk.host/v1alpha1
+    kind: FRPCIngress
+    executeHookOnEvent: ["Added","Modified","Deleted"]
+EOF
+}
+
+hooks::on_startup() {
+  node /utils/config-generator.js
+  screen -d -m /frp/frpc -c /frp/frpc.ini
+}
+
+hooks::on_event() {
+  node /utils/config-generator.js
+  pkill frpc
+  screen -d -m /frp/frpc -c /frp/frpc.ini
+}
+
+hooks::main() {
+  if [[ "$1" == "--config" ]];
+  then
+    hooks::on_config
+  else
+    [[ "$(jq -r '.[0].binding' ${BINDING_CONTEXT_PATH})" == "onStartup" ]] \
+      && hooks::on_startup
+    [[ "$(jq -r '.[0].type' ${BINDING_CONTEXT_PATH})" == "Event" ]] \
+      && hooks::on_event
+  fi
+}
+
+hooks::main "$@"
