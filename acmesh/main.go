ecbb47e739431ce635f03c3861fc6c240d2dc386
springhack <springhack@live.cn>
2022-11-14 21:08:13 +0800

    using secret on acmesh

--- acmesh/main.go
+++ acmesh/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "github.com/jetstack/cert-manager/pkg/issuer/acme/dns/util"
-  "k8s.io/client-go/kubernetes"
   "os"
+  "fmt"
+  "context"
+  "encoding/json"
 
-  extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-  "k8s.io/client-go/rest"
   "k8s.io/klog"
+  "k8s.io/client-go/rest"
+  "k8s.io/client-go/kubernetes"
+  extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 
-  "github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
   "github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
+  "github.com/jetstack/cert-manager/pkg/issuer/acme/dns/util"
+  "github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -36,26 +38,48 @@ type customDNSProviderSolver struct {
   client *kubernetes.Clientset
 }
 
-type customDNSProviderConfig struct {
-  TTL          *uint64                  `json:"ttl"`
-  DNSAPI       string                   `json:"dnsapi"`
-  Env          []string                 `json:"env"`
+type envSecretRef struct {
+  Name          string                  `json:"name"`
+  Namespace     string                  `json:"namespace"`
 }
+
+type customDNSProviderConfig struct {
+  TTL           *uint64                 `json:"ttl"`
+  DNSAPI        string                  `json:"dnsapi"`
+  EnvSecretRef  envSecretRef            `json:"env"`
+}
+
+type envFromSecret []string
 
 func (c *customDNSProviderSolver) Name() string {
   return "acmesh"
 }
 
-func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+func (c *customDNSProviderSolver) DoDNSAPI(action string, ch *v1alpha1.ChallengeRequest) error {
   cfg, err := loadConfig(ch.Config)
   if err != nil {
     klog.Errorf("Failed to log config %v: %v", ch.Config, err)
     return err
   }
 
+  envSecret, err := c.client.CoreV1().Secrets(cfg.EnvSecretRef.Namespace).Get(context.TODO(), cfg.EnvSecretRef.Name, metav1.GetOptions{})
+  if err != nil {
+    return err
+  }
+
+  envData, ok := envSecret.Data["env"]
+  if !ok {
+    return fmt.Errorf("no env in secret")
+  }
+
+  env := envFromSecret{}
+  if err := json.Unmarshal(envData, &env); err != nil {
+    return err
+  }
+
   procAttr := &os.ProcAttr{
     Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-    Env: cfg.Env,
+    Env: env,
   }
   process, err := os.StartProcess(acmeDelegate, []string{
     acmeDelegate, cfg.DNSAPI, "add", util.UnFqdn(ch.ResolvedFQDN), ch.Key,
@@ -68,26 +92,12 @@ func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
   return nil
 }
 
+func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+  return c.DoDNSAPI("add", ch)
+}
+
 func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-  cfg, err := loadConfig(ch.Config)
-  if err != nil {
-    klog.Errorf("Failed to log config %v: %v", ch.Config, err)
-    return err
-  }
-
-  procAttr := &os.ProcAttr{
-    Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-    Env: cfg.Env,
-  }
-  process, err := os.StartProcess(acmeDelegate, []string{
-    acmeDelegate, cfg.DNSAPI, "rm", util.UnFqdn(ch.ResolvedFQDN), ch.Key,
-  }, procAttr)
-  if err != nil {
-    return err
-  }
-
-  process.Wait()
-  return nil
+  return c.DoDNSAPI("rm", ch)
 }
 
 func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
