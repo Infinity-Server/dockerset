f09bc530a9f37daeeb2907b5dfe473b63940df04
springhack <springhack@live.cn>
2025-01-10 16:22:07 +0800

    update excalidraw storage

--- excalidraw/www/js/common.js
+++ excalidraw/www/js/common.js
@@ -1,121 +1,10 @@
 /*
  *  Author: SpringHack - springhack@live.cn
- *  Last modified: 2025-01-09 14:00:45
- *  Filename: js/common.js
+ *  Last modified: 2025-01-10 15:59:46
+ *  Filename: www/js/common.js
  *  Description: Created by SpringHack using vim automatically.
  */
-const createDocument = async () => {
-  const id = window.crypto.randomUUID();
-  const resp = await fetch('/storage', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      resultFormat: 'map',
-      transaction: [
-        {
-          statement: 'INSERT INTO storage (id, name, data) VALUES (:id, :name, :data)',
-          values: {
-            id,
-            name: 'untitled-1',
-            data: '{}'
-          }
-        }
-      ]
-    })
-  }).then(res => res.json());
-  if (resp?.results?.[0]?.success) {
-    return id;
-  }
-  return '';
-};
-
-const deleteDocument = async (id) => {
-  const resp = await fetch('/storage', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      resultFormat: 'map',
-      transaction: [
-        {
-          statement: 'DELETE FROM storage WHERE id = :id',
-          values: {
-            id
-          }
-        }
-      ]
-    })
-  }).then(res => res.json());
-  return resp?.results?.[0]?.success;
-};
-
-const getList = async (offset, count) => {
-  const rv = await fetch('/storage', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      resultFormat: 'map',
-      transaction: [
-        {
-          query: 'SELECT id, name from storage ORDER BY time DESC LIMIT :count OFFSET :offset',
-          values: {
-            count,
-            offset
-          }
-        }
-      ]
-    })
-  }).then(res => res.json());
-  return rv?.results?.[0]?.resultSet || [];
-};
-
-const searchList = async (keyword, count) => {
-  const rv = await fetch('/storage', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      resultFormat: 'map',
-      transaction: [
-        {
-          query: 'SELECT id, name from storage WHERE name like :keyword ORDER BY time DESC LIMIT :count',
-          values: {
-            count,
-            keyword: `%${keyword}%`
-          }
-        }
-      ]
-    })
-  }).then(res => res.json());
-  return rv?.results?.[0]?.resultSet || [];
-};
-
-const updateDocument = async (id, name, data = null) => {
-  const transaction = [
-    {
-      statement: `UPDATE storage SET time = CURRENT_TIMESTAMP, name = :name WHERE id = :id`,
-      values: {
-        id,
-        name
-      }
-    }
-  ];
-  if (data !== null) {
-    transaction.pop();
-    transaction.push({
-      statement: `UPDATE storage SET time = CURRENT_TIMESTAMP, data = :data WHERE id = :id`,
-      values: {
-        id,
-        data
-      }
-    });
-  }
+const doRequestStorage = async (transaction) => {
   const resp = await fetch('/storage', {
     method: 'POST',
     headers: {
@@ -126,37 +15,106 @@ const updateDocument = async (id, name, data = null) => {
       resultFormat: 'map'
     })
   }).then(res => res.json());
+  return resp;
+};
+
+const createDocument = async () => {
+  const id = window.crypto.randomUUID();
+  const resp = await doRequestStorage([
+    {
+      statement: '#CREATE_DOCUMENT',
+      values: {
+        id,
+        name: 'excalidraw-untitled'
+      }
+    }
+  ]);
+  return resp?.results?.[0]?.success && id || '';
+};
+
+const deleteDocument = async (id) => {
+  const resp = await doRequestStorage([
+    {
+      statement: '#DELETE_DOCUMENT',
+      values: {
+        id
+      }
+    }
+  ]);
+  return resp?.results?.[0]?.success;
+};
+
+const getList = async (offset, count) => {
+  const resp = await doRequestStorage([
+    {
+      query: '#GET_LIST',
+      values: {
+        count,
+        offset
+      }
+    }
+  ]);
+  return resp?.results?.[0]?.resultSet || [];
+};
+
+const searchList = async (keyword, count) => {
+  const resp = await doRequestStorage([
+    {
+      query: '#SEARCH_LIST',
+      values: {
+        count,
+        keyword: `%${keyword}%`
+      }
+    }
+  ]);
+  return resp?.results?.[0]?.resultSet || [];
+};
+
+const updateDocument = async (id, name, data = null) => {
+  const transaction = [
+    {
+      statement: '#UPDATE_DOCUMENT_NAME',
+      values: {
+        id,
+        name
+      }
+    }
+  ];
+  if (data !== null) {
+    transaction.pop();
+    transaction.push({
+      statement: '#UPDATE_DOCUMENT_DATA',
+      values: {
+        id,
+        data
+      }
+    });
+  }
+  const resp = await doRequestStorage(transaction);
   return resp?.results?.[0]?.success || false;
 };
 
 const getDocument = async (id) => {
-  const rv = await fetch('/storage', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      resultFormat: 'map',
-      transaction: [
-        {
-          query: 'SELECT * from storage WHERE id = :id',
-          values: {
-            id
-          }
-        }
-      ]
-    })
-  }).then(res => res.json());
-  return rv?.results?.[0]?.resultSet?.[0] || null;
+  const resp = await doRequestStorage([
+    {
+      query: '#GET_DOCUMENT',
+      values: {
+        id
+      }
+    }
+  ]);
+  return resp?.results?.[0]?.resultSet?.[0] || null;
 };
 
 const funcMap = {
   ignore: false
 };
+
 window.addEventListener('beforeunload', () => {
   if (funcMap.ignore) return;
   Object.values(funcMap).forEach(func => func instanceof Function && func.call());
 });
+
 document.addEventListener('visibilitychange', () => {
   if (funcMap.ignore) return;
   if (document.hidden) {
